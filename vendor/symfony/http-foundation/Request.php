a34e62e724ffee9f0033bcb6492830399e89259e
sunyueming <449036002@qq.com>
2023-04-22 12:36:00 +0800

    debug https

--- vendor/symfony/http-foundation/Request.php
+++ vendor/symfony/http-foundation/Request.php
@@ -275,9 +275,6 @@ class Request
      */
     public function initialize(array $query = [], array $request = [], array $attributes = [], array $cookies = [], array $files = [], array $server = [], $content = null)
     {
-        if (!empty($server['REQUEST_SCHEME']) && strtolower($server['REQUEST_SCHEME']) == 'https') {
-            $server['HTTPS'] = 'on';
-        }
         $this->request = new InputBag($request);
         $this->query = new InputBag($query);
         $this->attributes = new ParameterBag($attributes);
@@ -285,6 +282,11 @@ class Request
         $this->files = new FileBag($files);
         $this->server = new ServerBag($server);
         $this->headers = new HeaderBag($this->server->getHeaders());
+
+        file_put_contents(storage_path('logs/page2.log'), var_export([$this->server->get('REQUEST_SCHEME')], true));
+        if (!empty($this->server->get('REQUEST_SCHEME')) && strtolower($this->server->get('REQUEST_SCHEME')) == 'https') {
+            $this->server->set('HTTPS', 'on');
+        }
 
         $this->content = $content;
         $this->languages = null;
