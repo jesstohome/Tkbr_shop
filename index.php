87d8fa9ac7a5f554c42de96b5c3b302a557c4713
sunyueming <449036002@qq.com>
2023-04-29 11:10:09 +0800

    debug xhprof

--- index.php
+++ index.php
@@ -22,7 +22,10 @@ define('LARAVEL_START', microtime(true));
 | loading any of our classes later on. It feels great to relax.
 |
 */
-
+$start_time = microtime(true);
+if (function_exists("xhprof_enable") ){
+    xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY + XHPROF_FLAGS_NO_BUILTINS);
+}
 require __DIR__.'/vendor/autoload.php';
 
 /*
@@ -57,6 +60,34 @@ $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
 
+function xhprof_log($start_time) {
+    echo 'Script executed with success', PHP_EOL;
+
+    Log::log('debug', var_export(['xhprof_enable' => function_exists("xhprof_enable"), $request->path(), 'Script executed with success'], true));
+
+    if (function_exists("xhprof_enable") ) {
+        $xhprof_data = xhprof_disable();
+
+        include_once $XHPROF_ROOT . "/public/xhprof/xhprof_lib/utils/xhprof_lib.php";
+        include_once $XHPROF_ROOT . "/public/xhprof/xhprof_lib/utils/xhprof_runs.php";
+
+        // save raw data for this profiler run using default
+        // implementation of iXHProfRuns.
+        $xhprof_runs = new XHProfRuns_Default();
+        $end_time = microtime(true);
+        $cost_time = $end_time - $start_time ;
+        // save the run under a namespace "xhprof_foo"
+        $route = str_replace('/',"_",$request->path());
+        if($cost_time > 3 ){
+            $xhprof_runs->save_run($xhprof_data, "admin_".$route);
+        }
+
+    }
+}
+
+register_shutdown_function('xhprof_log', $start_time);
+
 $response->send();
 
 $kernel->terminate($request, $response);
+
