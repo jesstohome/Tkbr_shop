b29302ea06376d980b4769d8b277221319a657bf
sunyueming <449036002@qq.com>
2023-05-04 23:05:07 +0800

    HTPAY 代付 定死一个手机号

--- app/Http/Controllers/Payment/HtpayController.php
+++ app/Http/Controllers/Payment/HtpayController.php
@@ -155,7 +155,7 @@ class HtpayController extends Controller
             'bank_num' => $shop->bank_acc_no ?: $user->bank_acc_no, //银行卡号
             'account_name' => $shop->bank_acc_name ?: $user->bank_acc_name, //银行卡账户名
             'customer_email' => $user->email, //用户邮箱
-            'customer_mobile' => "", //用户手机号码格式要正确
+            'customer_mobile' => "91829732132", //用户手机号码格式要正确
             'notify_url' => route('htpay.notify'), //异步回调地址不带参数
             'bank_name' => $shop->bank_name ?: $user->bank_name, //银行名称
             'country_id' => "2", //1印度 2印尼
