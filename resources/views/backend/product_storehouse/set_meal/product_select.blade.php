1e29edf737af28eae861e3c465302d8d8b2cc354
sunyueming <449036002@qq.com>
2023-05-08 21:30:09 +0800

    产品库套餐

--- resources/views/backend/product_storehouse/set_meal/product_select.blade.php
+++ resources/views/backend/product_storehouse/set_meal/product_select.blade.php
@@ -1,7 +1,7 @@
 <select name="product_ids[]" id="product_ids" class="form-control aiz-selectpicker" multiple required data-placeholder="{{ translate('Choose Products') }}" data-live-search="true" data-selected-text-format="count">
 @if(!empty($category_id))
-@foreach(\App\Models\Product::where('category_id', $category_id)->orderBy('id', 'desc')->select(["id", "name"])->get() as $product)
-    <option value="{{$product->id}}">{{ $product->getTranslation('name') }}</option>
+@foreach(\App\Models\Product::where('category_id', $category_id)->orderBy('id', 'desc')->select(["id", "name", 'unit_price'])->get() as $product)
+    <option value="{{$product->id}}">({{single_price($product->unit_price)}}) {{ $product->getTranslation('name') }}</option>
 @endforeach
 @endif
 </select>
