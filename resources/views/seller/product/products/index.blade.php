ab126213e6379475a1fbd052a44ac8cb2f8d30c8
sunyueming <449036002@qq.com>
2023-05-13 21:15:02 +0800

    卖家产品的排版优化

--- resources/views/seller/product/products/index.blade.php
+++ resources/views/seller/product/products/index.blade.php
@@ -60,14 +60,14 @@
                         </th>
                         <th width="30%">{{ translate('Name')}}</th>
                         <th data-breakpoints="md">{{ translate('Category')}}</th>
-                        <th >{{ translate('Current Qty')}}</th>
+                        <th data-breakpoints="md">{{ translate('Current Qty')}}</th>
                         <th data-breakpoints="md">{{ translate('Pick Up Price')}}</th>
-                        <th data-breakpoints="md">{{ translate('Base Price')}}</th>
-                        <th data-breakpoints="md">{{ translate('Profit')}}</th>
+                        <th>{{ translate('Base Price')}}</th>
+                        <th>{{ translate('Profit')}}</th>
                         @if(get_setting('product_approve_by_admin') == 1)
                             <th data-breakpoints="md">{{ translate('Approval')}}</th>
                         @endif
-                        <th>{{ translate('Published')}}</th>
+                        <th data-breakpoints="md">{{ translate('Published')}}</th>
                         <th data-breakpoints="md">{{ translate('Featured')}}</th>
                         <th data-breakpoints="md" class="text-right">{{ translate('Options')}}</th>
                     </tr>
