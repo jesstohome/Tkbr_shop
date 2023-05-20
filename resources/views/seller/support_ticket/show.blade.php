d7ca003e2c9757ad0980ca550c70b2419aea1d07
sunyueming <449036002@qq.com>
2023-05-20 12:54:27 +0800

    聊天样式

--- resources/views/seller/support_ticket/show.blade.php
+++ resources/views/seller/support_ticket/show.blade.php
@@ -198,6 +198,7 @@
     <script type="text/javascript">
         $(document).ready(function () {
             $(".card.chat").height(document.body.clientHeight - 75)
+            $(".aiz-main-content").height(document.body.clientHeight - 75)
         });
     </script>
 @endsection
