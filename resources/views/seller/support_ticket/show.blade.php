f52fcfce37ccddd18c19f7ca79b967dc5250eb25
sunyueming <449036002@qq.com>
2023-05-20 11:18:37 +0800

    Revert "聊天样式"
    This reverts commit 8f0390f4fd308a7eb95dd27f0f95647aa666cf8e.

--- resources/views/seller/support_ticket/show.blade.php
+++ resources/views/seller/support_ticket/show.blade.php
@@ -68,8 +68,7 @@
     }
 
     .card.chat {
-        /*height: 100%;*/
-        height: 46rem;
+        height: 100%;
         margin-bottom: 0;
     }
 
@@ -198,7 +197,7 @@
 
     <script type="text/javascript">
         $(document).ready(function () {
-            // $(".card.chat").height(window.screen.availHeight - 75)
+            $(".card.chat").height(window.screen.availHeight - 75)
         });
     </script>
 @endsection
