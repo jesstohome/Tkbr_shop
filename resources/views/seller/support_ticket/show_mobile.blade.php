2fc8dbfd942007dab0dc9826582f81d5712461b3
sunyueming <449036002@qq.com>
2023-05-24 14:12:45 +0800

    0523文档

--- resources/views/seller/support_ticket/show_mobile.blade.php
+++ resources/views/seller/support_ticket/show_mobile.blade.php
@@ -180,7 +180,7 @@
                             <div class="file-preview box sm"></div>
                         </div>
                         <div class="col-8">
-                            <input class="form-control" type="text" name="reply" value="" enterkeyhint="send"/>
+                            <input class="form-control" type="text" name="reply" value="" enterkeyhint="go"/>
                         </div>
                         <div class="col-2">
                             <svg t="1684411791031" onclick="submit_reply('pending')" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="18723" width="20" height="20"><path d="M512 512m-448 0a448 448 0 1 0 896 0 448 448 0 1 0-896 0Z" fill="#608BE9" p-id="18724"></path><path d="M192 302m32 0l576 0q32 0 32 32l0 356q0 32-32 32l-576 0q-32 0-32-32l0-356q0-32 32-32Z" fill="#EAEDF5" p-id="18725"></path><path d="M224 722h576c17.673 0 32-14.327 32-32v-58C660.96 493.333 554.294 424 512 424c-42.294 0-148.96 69.333-320 208v58c0 17.673 14.327 32 32 32z" fill="#CCDAF7" p-id="18726"></path><path d="M224 302h576c17.673 0 32 14.327 32 32v58C651.35 517.333 544.683 580 512 580c-32.683 0-139.35-62.667-320-188v-58c0-17.673 14.327-32 32-32z" fill="#FFFFFF" p-id="18727"></path></svg>
