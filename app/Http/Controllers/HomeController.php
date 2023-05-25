9a42c56a8d19022a26f44dd174987d794b0e0c6f
sunyueming <449036002@qq.com>
2023-05-25 16:02:26 +0800

    登录后再登录的跳转

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -200,6 +200,13 @@ class HomeController extends Controller
     public function login()
     {
         if (Auth::check()) {
+            if (auth()->user()->user_type == 'admin' || auth()->user()->user_type == 'staff') {
+                \CoreComponentRepository::instantiateShopRepository();
+                return redirect()->route('admin.dashboard');
+            } elseif (auth()->user()->user_type == 'seller') {
+                return redirect()->route('seller.dashboard');
+            }
+
             return redirect()->route('home');
         }
         return view('frontend.user_login');
