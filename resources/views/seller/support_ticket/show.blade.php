cbf9056d63287c3f4a5cd8137dbe4c341f2e004d
sunyueming <449036002@qq.com>
2023-05-25 15:16:40 +0800

    图片也放在聊天框中

--- resources/views/seller/support_ticket/show.blade.php
+++ resources/views/seller/support_ticket/show.blade.php
@@ -45,7 +45,6 @@
     ul.ticket li .time {
         right: 1rem;
         position: absolute;
-        bottom: 0.2rem;
         text-align: right;
         padding: 0;
         margin: 0;
@@ -142,20 +141,22 @@
                                     <div class="comment-header">
                                         <span class="text-bold h6 text-muted title">
                                             @php echo $ticketreply->reply; @endphp
+                                            @if($ticketreply->files)
+                                                <div class="images {{$ticketreply->user->id == Auth::id() ? 'mine' : ''}}">
+                                                @foreach ((explode(",",$ticketreply->files)) as $key => $file)
+                                                    @php $file_detail = \App\Models\Upload::where('id', $file)->first(); @endphp
+                                                    @if($file_detail != null)
+                                                        <img src="{{ static_asset('assets/img/placeholder.jpg') }}" data-src="{{ uploaded_asset($file) }}" onclick="previewImg(this)" class="mr-3 lazyload size-100px img-fit rounded" alt="Image">
+                                                    @endif
+                                                @endforeach
+                                            </div>
+                                            @endif
                                             <p class="text-muted text-sm fs-11 time">{{date('m-d H:i', strtotime($ticketreply->created_at))}}</p>
                                         </span>
                                     </div>
                                 </div>
                             </div>
                             @endif
-                            <div class="images {{$ticketreply->user->id == Auth::id() ? 'mine' : ''}}">
-                                @foreach ((explode(",",$ticketreply->files)) as $key => $file)
-                                    @php $file_detail = \App\Models\Upload::where('id', $file)->first(); @endphp
-                                    @if($file_detail != null)
-                                        <img src="{{ static_asset('assets/img/placeholder.jpg') }}" data-src="{{ uploaded_asset($file) }}" onclick="previewImg(this)" class="mr-3 lazyload size-100px img-fit rounded" alt="Image">
-                                    @endif
-                                @endforeach
-                            </div>
                         </li>
                     @endforeach
                 </ul>
