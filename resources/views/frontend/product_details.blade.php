fd97d9f665416717ddcbc60899cb55bba6b20f53
sunyueming <449036002@qq.com>
2023-05-30 00:16:16 +0800

    fix sql error

--- resources/views/frontend/product_details.blade.php
+++ resources/views/frontend/product_details.blade.php
@@ -262,7 +262,10 @@
                                         <div class="row no-gutters">
                                             <div class="col-sm-2">
                                                 <div class="opacity-50 my-2">
-                                                    {{ \App\Models\Attribute::query()->where("name", $attrs[$key] ?? 2)->first()->getTranslation('name') }}:
+                                                    @php
+                                                    $attribute = \App\Models\Attribute::query()->where("name", $attrs[$key] ?? 2)->first();
+                                                    @endphp
+                                                    {{ $attribute ? $attribute->getTranslation('name') : ''}}:
                                                 </div>
                                             </div>
                                             <div class="col-sm-10">
