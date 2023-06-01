cdd1f5de28f8d1bb577fbe621a59fce27e9c9a5a
sunyueming <449036002@qq.com>
2023-06-01 23:29:42 +0800

    修复，当组合没有规格时，直接显示缺货

--- resources/views/frontend/product_details.blade.php
+++ resources/views/frontend/product_details.blade.php
@@ -275,7 +275,7 @@
                                                             <input type="radio"
                                                                 name="attribute_id_{{ $choice->attribute_id }}"
                                                                 value="{{ $value }}"
-                                                                @if ($key == 0) checked @endif>
+                                                                >
                                                             <span
                                                                 class="aiz-megabox-elem rounded d-flex align-items-center justify-content-center py-2 px-3 mb-2">
                                                                 {{ $value }}
