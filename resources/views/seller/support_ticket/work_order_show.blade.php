3e7e6885e0ae407427fafef5c4c0fbf098cc3a67
sunyueming <449036002@qq.com>
2023-06-02 22:31:59 +0800

    更换翻译

--- resources/views/seller/support_ticket/work_order_show.blade.php
+++ resources/views/seller/support_ticket/work_order_show.blade.php
@@ -127,7 +127,7 @@
             <div class="text-center text-md-left">
                 <h5 class="mb-md-0 h5" style="height: 25px;overflow: hidden;">{{$ticket->order->details[0]->product->getTranslation('name')}}</h5>
                <div class="">
-                   <span> {{ translate("The manufacturer has paid a bail") }} </span>
+                   <span> {{ translate("The manufacturer has paid a security deposit") }} </span>
                </div>
                 <div class="">
                     <span> {{ translate('Order No') }}: {{$ticket->order->code}} </span>
