e6a4c7bddccbe44fa855079139053b77cc0359f6
sunyueming <449036002@qq.com>
2023-06-03 15:11:46 +0800

    winpay支付

---
+++ app/Http/Controllers/Payment/WinpayController.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Http\Controllers\Payment;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class WinpayController extends Controller
+{
+
+    public function pay(Request $request) {
+
+    }
+
+    public function daifu_pay ($withdrawRequest) {
+
+    }
+
+    public function notify(Request $request) {
+
+    }
+}
