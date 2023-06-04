a7cc01521045750c0d5800f5e240bf91e3a305ca
sunyueming <449036002@qq.com>
2023-06-04 21:47:09 +0800

    env文件存的时候不带引号

--- app/Http/Controllers/OTPController.php
+++ app/Http/Controllers/OTPController.php
@@ -72,11 +72,16 @@ class OTPController extends Controller
     {
         $path = base_path('.env');
         if (file_exists($path)) {
-            $val = '"'.trim($val).'"';
+            $val = trim($val);
             if(is_numeric(strpos(file_get_contents($path), $type)) && strpos(file_get_contents($path), $type) >= 0){
-                file_put_contents($path, str_replace(
-                    $type.'="'.env($type).'"', $type.'='.$val, file_get_contents($path)
-                ));
+                $old_content = file_get_contents($path);
+                $new_content = str_replace(
+                    $type.'="'.env($type).'"', $type.'='.$val, $old_content
+                );
+                $new_content = str_replace(
+                    $type.'='.env($type), $type.'='.$val, $new_content
+                );
+                file_put_contents($path, $new_content);
             }
             else{
                 file_put_contents($path, file_get_contents($path)."\r\n".$type.'='.$val);
