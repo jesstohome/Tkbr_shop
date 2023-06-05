1df049d37d4ceee733cf294179bd61d49c8fd692
sunyueming <449036002@qq.com>
2023-06-05 14:57:28 +0800

    修复 不能提现问题

--- app/Http/Controllers/Seller/SellerWithdrawRequestController.php
+++ app/Http/Controllers/Seller/SellerWithdrawRequestController.php
@@ -110,7 +110,7 @@ class SellerWithdrawRequestController extends Controller
                 $seller_withdraw_request->bloc_id = $user->bloc_id;
                 $seller_withdraw_request->staff_id = $user->staff_id;
                 $seller_withdraw_request->cur_select_country_code = $request->country_code;
-                $seller_withdraw_request->currency = $request->currency;
+                $seller_withdraw_request->cur_currency_code = $request->currency;
                 $seller_withdraw_request->amount = $request->amount;
                 $seller_withdraw_request->message = $request->message;
                 $seller_withdraw_request->status = '0';
