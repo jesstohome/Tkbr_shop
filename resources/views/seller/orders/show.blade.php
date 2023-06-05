a90faef474ee98edf5900a61e23f043b9c60da12
sunyueming <449036002@qq.com>
2023-06-05 20:37:16 +0800

    卖家后台-钱包余额不足时，会跳转到钱包充值页面。改成不跳转到钱包充值页面。

--- resources/views/seller/orders/show.blade.php
+++ resources/views/seller/orders/show.blade.php
@@ -493,8 +493,8 @@
                     AIZ.plugins.notify('danger', data.message ? data.message : '{{ translate('Something went wrong') }}');
                     if (!!data.type && data.type == 'balance_insufficient') {
                         setTimeout(function () {
-                            location.href = '{{route('seller.money_withdraw_requests.index')}}?auto_show_recharge=1'
-                        }, 1000)
+                            $("#payment_for_storehouse_modal").modal("hide");
+                        }, 100)
                     }
                 }
             });
