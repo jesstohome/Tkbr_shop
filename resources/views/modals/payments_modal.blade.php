e4fa105df8b2561d051089b9dbb7ad798a3d6906
sunyueming <449036002@qq.com>
2023-06-05 22:06:06 +0800

    翻译

--- resources/views/modals/payments_modal.blade.php
+++ resources/views/modals/payments_modal.blade.php
@@ -86,7 +86,7 @@
                         </div>
                         @else
                         <div class="col-md-12">
-                            <a href="javascript:void(0);" id="wallet-link" onclick="$('#payment_for_storehouse_modal').modal('show')" class="btn btn-primary mt-2">Wallet</a>
+                            <a href="javascript:void(0);" id="wallet-link" onclick="$('#payment_for_storehouse_modal').modal('show')" class="btn btn-primary mt-2">{{translate('Wallet')}}</a>
 
                             @if(count(\App\Models\ManualPaymentMethod::listByBloc(0, $order->shop->bloc_id)))
                                 <a href="javascript:void(0);" id="Manual-link" onclick="show_make_wallet_recharge_modal(3)" class="btn btn-primary mt-2">{{translate('Manual transfer')}}</a>
