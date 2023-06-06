f710661ea5debf3b28db4bdd6582b3ca91a2d162
sunyueming <449036002@qq.com>
2023-06-06 14:47:09 +0800

    卖家的评论红点

--- app/Http/Controllers/Seller/ReviewController.php
+++ app/Http/Controllers/Seller/ReviewController.php
@@ -24,7 +24,7 @@ class ReviewController extends Controller
             $review->save();
         }
 
-        \Redis::del('new_review_tip');
+        del_plus('new_review_tip:seller');
 
         return view('seller.reviews', compact('reviews'));
     }
