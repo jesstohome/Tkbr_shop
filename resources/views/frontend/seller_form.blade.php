31330258ee5fdbb0c5b857797856cf48f15f854f
sunyueming <449036002@qq.com>
2023-06-06 21:14:46 +0800

    注册时必须填邀请码和店铺名称

--- resources/views/frontend/seller_form.blade.php
+++ resources/views/frontend/seller_form.blade.php
@@ -150,6 +150,14 @@
                 AIZ.plugins.notify('danger', '{{translate('The username is required and cannot be empty')}}');
                 return false;
             }
+            if ($("input[name=shop_name]").val().trim() === '') {
+                AIZ.plugins.notify('danger', '{{translate('The shopname is required and cannot be empty')}}');
+                return false;
+            }
+            if ($("input[name=staff_invite_code]").val().trim() === '') {
+                AIZ.plugins.notify('danger', '{{translate('The invite code is required and cannot be empty')}}');
+                return false;
+            }
             if ($("input[name=email]").val().trim() === '') {
                 AIZ.plugins.notify('danger', '{{translate('The email is required and cannot be empty')}}');
                 return false;
