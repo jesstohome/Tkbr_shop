d87f2581f4ba91b2a7c2fb22f0aa737adf174e23
sunyueming <449036002@qq.com>
2023-06-12 11:03:48 +0800

    首页--通过点击“参观商店”进入的情况下，点击“快速链接”页面都会报错

--- resources/views/frontend/inc/footer.blade.php
+++ resources/views/frontend/inc/footer.blade.php
@@ -97,7 +97,7 @@
                         @if ( get_setting('widget_one_labels',null,App::getLocale()) !=  null )
                             @foreach (json_decode( get_setting('widget_one_labels',null,App::getLocale()), true) as $key => $value)
                             <li class="mb-2">
-                                <a href="{{ json_decode( get_setting('widget_one_links'), true)[$key] }}" class="opacity-50 hov-opacity-100 text-reset">
+                                <a href="/{{ json_decode( get_setting('widget_one_links'), true)[$key] }}" class="opacity-50 hov-opacity-100 text-reset">
                                     {{ $value }}
                                 </a>
                             </li>
