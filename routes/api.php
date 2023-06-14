d24d057516636748e340cd2f8b247d6707c5bbfd
sunyueming <449036002@qq.com>
2023-06-14 20:26:58 +0800

    FORCE_HTTPS

--- routes/api.php
+++ routes/api.php
@@ -63,7 +63,7 @@ Route::group(['prefix' => 'v2', 'middleware' => ['app_language']], function() {
     Route::get('categories/home', 'App\Http\Controllers\Api\V2\CategoryController@home');
     Route::get('categories/top', 'App\Http\Controllers\Api\V2\CategoryController@top');
     Route::apiResource('categories', 'App\Http\Controllers\Api\V2\CategoryController')->only('index');
-    Route::get('sub-categories/{id}', 'App\Http\Controllers\Api\V2\SubCategoryController@index')->name('subCategories.index');
+//    Route::get('sub-categories/{id}', 'App\Http\Controllers\Api\V2\SubCategoryController@index')->name('subCategories.index');
 
     Route::apiResource('colors', 'App\Http\Controllers\Api\V2\ColorController')->only('index');
 
