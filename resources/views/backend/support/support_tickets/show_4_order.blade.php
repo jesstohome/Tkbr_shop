78587bdd1329af2ff9355b4ae5a3b87c861df02a
mingming <449036002@qq.com>
2023-06-19 15:52:21 +0800

    行政后台，客服工单和付款单工单的聊天界面，发送的信息增加显示 已读/未读

--- resources/views/backend/support/support_tickets/show_4_order.blade.php
+++ resources/views/backend/support/support_tickets/show_4_order.blade.php
@@ -143,7 +143,7 @@
                     </div>
                 </div>
                 <ul class="list-group list-group-flush ticket">
-                    @foreach($ticketreplies as $ticketreply)
+                    @foreach($ticket_replies as $ticketreply)
                         @if(empty($ticketreply->user_id)) @continue @endif
                         <li class="list-group-item px-0 {{-1 == $ticketreply->user_id || $ticketreply->user_id == Auth::id() ? 'mine' : ''}} {{$ticketreply->read ? 'is-read' : 'un-read'}}" data-id="{{$ticketreply->id}}">
                             <div class="media">
