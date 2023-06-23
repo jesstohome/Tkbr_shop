0e07ef2d035e54166200e68879c2939f77c4b637
mingming <449036002@qq.com>
2023-06-23 23:32:10 +0800

    商城首页优化：1.首页商品每刷新一次或重新进入所有商品都需随机展示。2.所有类别：分类/子分类商品每刷新一次或者重新进入都需随机展示

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -178,6 +178,9 @@
                             </h3>
                         </div>
                         <div class="aiz-carousel gutters-10 half-outside-arrow" data-items="6" data-xl-items="5" data-lg-items="4"  data-md-items="3" data-sm-items="2" data-xs-items="2" data-arrows='true'>
+                            @php
+                            $newest_products = $newest_products->shuffle()
+                            @endphp
                             @foreach ($newest_products as $key => $new_product)
                             <div class="carousel-box">
                                 @include('frontend.partials.product_box_1',['product' => $new_product])
