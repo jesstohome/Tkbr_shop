83ee5c5446277709e3decd156674211cc477013d
mingming <449036002@qq.com>
2023-06-24 19:10:39 +0800

    {成交订单}-所有订单-收银员订单-卖家订单-内部订单-定时订单 （这几个菜单优化）(详见文档)

--- resources/views/backend/sales/all_orders/index.blade.php
+++ resources/views/backend/sales/all_orders/index.blade.php
@@ -245,9 +245,15 @@
                                 <a href="javascript:void(0);" class="btn btn-soft-danger btn-icon btn-circle btn-sm" onclick="AIZ.plugins.notify('danger', '{{translate('The order has been picked up and cannot be deleted')}}');return false;" title="{{ translate('Delete') }}">
                                     <i class="las la-trash"></i>
                                 </a>
+                                <a href="javascript:void(0);" class="btn btn-soft-danger btn-icon btn-circle btn-sm" onclick="AIZ.plugins.notify('danger', '{{translate('The order has been picked up and cannot be cancel')}}');return false;" title="{{ translate('Cancel') }}">
+                                    <i class="las la-tint"></i>
+                                </a>
                                 @else
                                 <a href="#" class="btn btn-soft-danger btn-icon btn-circle btn-sm confirm-delete" data-href="{{route('orders.destroy', $order->id)}}}}" title="{{ translate('Delete') }}">
                                     <i class="las la-trash"></i>
+                                </a>
+                                <a href="#" class="btn btn-soft-danger btn-icon btn-circle btn-sm confirm-cancel" data-href="{{route('orders.cancel', $order->id)}}}}" title="{{ translate('Delete') }}">
+                                    <i class="las la-tint"></i>
                                 </a>
                             @endif
 
