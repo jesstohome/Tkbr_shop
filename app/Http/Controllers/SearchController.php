1d71f37a9c76f7bd6064290307fe845fc7eb023a
mingming <449036002@qq.com>
2023-06-24 10:47:31 +0800

    商城首页优化：1.首页商品每刷新一次或重新进入所有商品都需随机展示。2.所有类别：分类/子分类商品每刷新一次或者重新进入都需随机展示

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -121,6 +121,14 @@ class SearchController extends Controller
             }
         }
 
+        if (empty($request->page) || $request->page == 1) {
+            // 第一页随机取产品
+            $productIds = $products->pluck("id")->toArray();
+            shuffle($productIds);
+            $productIds = array_slice($productIds, 0, 12);
+            $products->whereIn('id', $productIds);
+        }
+
         $products = filter_products($products)->with('taxes')->paginate(12)->appends(request()->query());
 
         return view('frontend.product_listing', compact('products', 'query', 'category_id', 'brand_id', 'sort_by', 'seller_id', 'min_price', 'max_price', 'attributes', 'selected_attribute_values', 'colors', 'selected_color'));
