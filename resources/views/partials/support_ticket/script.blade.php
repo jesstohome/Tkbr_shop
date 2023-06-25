66126c88247e853427afa205f08443a96a5447f6
mingming <449036002@qq.com>
2023-06-25 21:53:45 +0800

    debug图片上传

--- resources/views/partials/support_ticket/script.blade.php
+++ resources/views/partials/support_ticket/script.blade.php
@@ -225,6 +225,7 @@
                 // var reader = new FileReader();
 
                 var imageURL = URL.createObjectURL(selectedFile);
+                alert(imageURL);
                 if (imageURL) {
                     addToPreview(imageURL, 'remove-attachment-mobile');
                     $("div.message.send").show();
