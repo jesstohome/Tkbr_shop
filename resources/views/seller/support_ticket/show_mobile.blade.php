14a01264130d5aebcdc6228cbbd10eda27cc5554
mingming <449036002@qq.com>
2023-06-25 20:56:51 +0800

    fix: 第4条：安卓H5端，工单客服+工单付款客服，选择相片时，如果没有点击在相片右上角小圆圈内，就会出现多一个步骤去点击确认。改为：和APP版一样，只要点击到该相片，就直接被选上

--- resources/views/seller/support_ticket/show_mobile.blade.php
+++ resources/views/seller/support_ticket/show_mobile.blade.php
@@ -56,7 +56,7 @@
                 <input type="hidden" name="ticket_id" value="{{$ticket->id}}" required>
                 <input type="hidden" name="user_id" value="{{$ticket->user_id}}">
                 <input type="hidden" name="attachments" class="selected-files">
-                <input type="file" id="fileInput" name="file" style="display: none;" accept="image/*" multiple />
+                <input type="file" id="fileInput" name="file" style="display: none;" accept="image/*" />
 
                 <input class="input" placeholder="{{translate('Please enter your question')}}" name="reply" onkeyup="toggleSendBtn(this)" onkeydown="submitReply()" enterkeyhint="send" />
             </form>
