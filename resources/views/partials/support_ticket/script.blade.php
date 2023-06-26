ebd918e7a0a8c4ae7e1799e63b2d4bc58318c381
mingming <449036002@qq.com>
2023-06-26 11:18:28 +0800

    Revert "debug图片上传"
    This reverts commit 16924c7b277a0071b189557baf00e4afd0384324.

--- resources/views/partials/support_ticket/script.blade.php
+++ resources/views/partials/support_ticket/script.blade.php
@@ -226,6 +226,7 @@
 
                 var imageURL = URL.createObjectURL(selectedFile);
                 AIZ.extra.log({type:selectedFile.type, name: selectedFile.name, imageURL:imageURL});
+                alert(imageURL);
                 if (imageURL) {
                     addToPreview(imageURL, 'remove-attachment-mobile');
                     $("div.message.send").show();
