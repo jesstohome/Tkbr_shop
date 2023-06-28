73cf69ee1ce8cf9126aad71cd46d14dcc76d99fb
mingming <449036002@qq.com>
2023-06-28 19:44:39 +0800

    行政后台：编辑卖家信息把名字设为可更改，卖家后台：不可更改。

--- resources/views/backend/sellers/edit.blade.php
+++ resources/views/backend/sellers/edit.blade.php
@@ -19,7 +19,7 @@
                 <div class="form-group row">
                     <label class="col-sm-3 col-from-label" for="name">{{translate('Name')}}</label>
                     <div class="col-sm-9">
-                        <input type="text" placeholder="{{translate('Name')}}" id="name" name="name" class="form-control" value="{{$shop->user->name}}" required readonly>
+                        <input type="text" placeholder="{{translate('Name')}}" id="name" name="name" class="form-control" value="{{$shop->user->name}}" required>
                     </div>
                 </div>
                 <div class="form-group row">
