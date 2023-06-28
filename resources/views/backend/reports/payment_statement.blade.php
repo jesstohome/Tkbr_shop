1a9314535369900175fcaae3bf888e2114548bee
mingming <449036002@qq.com>
2023-06-28 15:03:10 +0800

    优化卖家下拉列表SQL

--- resources/views/backend/reports/payment_statement.blade.php
+++ resources/views/backend/reports/payment_statement.blade.php
@@ -13,21 +13,7 @@
         <div class="card">
             <form action="{{ route('payment-statement.index') }}" method="GET">
                 <div class="card-header row gutters-5">
-                    <div class="col-lg-2">
-                        <div class="form-group mb-0">
-                            @php
-                                $sellers = filter_by_bloc(App\Models\User::where('user_type', '=', 'seller'))->join("shops", "shops.user_id", '=', 'users.id')->select("users.*", "shops.name as shop_name")->get();
-                            @endphp
-                            <select class="form-control form-control-sm aiz-selectpicker mb-2 mb-md-0" id="seller_id" name="seller_id" data-live-search="true">
-                                <option value="">{{ translate('All Sellers') }}</option>
-                                @foreach ($sellers as $key => $seller)
-                                    <option value="{{ $seller->id }}" @if ($seller->id == $seller_id) selected @endif>
-                                        {{ $seller->shop_name }} ({{ $seller->email }})
-                                    </option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
+                    @include('backend.partials.filters.seller')
                     <div class="col-lg-2">
                         <div class="form-group mb-0">
                             <input type="text" class="form-control" id="order_code" name="order_code" @isset($order_code) value="{{ $order_code }}" @endisset placeholder="{{ translate('Type Order code & hit Enter') }}">
