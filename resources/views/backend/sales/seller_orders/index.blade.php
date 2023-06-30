0d88ac6f9aa04af7bd880a2849443419254b105c
mingming <449036002@qq.com>
2023-06-30 10:31:27 +0800

    卖家订单重置按钮

--- resources/views/backend/sales/seller_orders/index.blade.php
+++ resources/views/backend/sales/seller_orders/index.blade.php
@@ -39,6 +39,7 @@
             <div class="col-auto">
                 <div class="form-group mb-0">
                     <button type="submit" class="btn btn-primary">{{ translate('Filter') }}</button>
+                    <button class="btn btn-md btn-primary" type="reset" onclick="reset_form()">重置</button>
                 </div>
             </div>
         </div>
