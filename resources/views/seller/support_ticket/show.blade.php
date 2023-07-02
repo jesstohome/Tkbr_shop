43701f07b6e1284d0f8541fd2c5fb64066a4fb09
mingming <449036002@qq.com>
2023-07-02 15:14:09 +0800

    卖家PC端UI优化

--- resources/views/seller/support_ticket/show.blade.php
+++ resources/views/seller/support_ticket/show.blade.php
@@ -1,209 +1,108 @@
 @extends('seller.layouts.app')
+<link rel="stylesheet" href="{{ static_asset('assets/css/chat-pc.css?v=1.1.0') }}">
 <style type="text/css">
-    div.aiz-main-content {
-        width: 80%;
-        margin: auto;
+    .kefuroot {
+        width: 100% !important;
     }
-    div.footer-site-name {
-        display: none;
+    .aiz-main-content .px-15px {
+        padding-left: 0;
+        padding-right: 0;
     }
-    ul.ticket {
-        height: calc(100% - 118px);
-        overflow-y: scroll;
-        margin-top: 75px;
-    }
-    ul.ticket, ul.ticket li {
-        background-color: #ebedf2;
-    }
-    ul.ticket li .comment-header {
-        display: flex;
-        align-items: flex-start;
-        flex-direction: column;
-    }
-    ul.ticket li .title {
-        position: relative;
-        max-width: 55vw;
-        background-color: white;
-        padding: 5px 10px;
-
-        border-radius: 15px;
-
-        word-wrap: break-word;
-        overflow-wrap: break-word;
-        min-width: 8rem;
-        width: fit-content;
-        padding-bottom: 1.5rem;
-        line-height: 2rem;
-    }
-    ul.ticket li.mine .title {
-        background-color: #d9fdd3;
-    }
-    ul.ticket li .title p {
-        /*width: inherit;*/
-        width: -webkit-fill-available;
-    }
-    ul.ticket li .time {
-        right: 1rem;
-        position: absolute;
-        text-align: right;
-        padding: 0;
-        margin: 0;
-        color: #8b8b8b !important;
+    .aiz-main-content .px-lg-25px {
+        padding-left: 0;
+        padding-right: 0;
     }
 
-    ul.ticket li .comment-header {
-        margin-left: 0.5rem;
-    }
-    ul.ticket li.mine .comment-header {
-        display: flex;
-        justify-content: flex-end;
-        align-items: flex-end;
-        margin-right: 0.5rem;
-    }
-
-    div.images.mine {
-        display: flex;
-        justify-content: flex-end;
-    }
-
-    .note-editable.card-block {
-        height: 80px !important;
-    }
-
-    .card.chat {
-        height: 80vh;
-        margin-bottom: 0;
-    }
-
-    .card .card-header {
-        position: absolute !important;
-        min-height:auto;
-        top: 0;
-        z-index: 10;
+    .notice {
         left: 0;
+    }
+
+    .botRoot {
         right: 0;
-        background-color: white;
-        height: 75px;
-    }
-    .card .card-body {
-        /*padding: 0 !important;*/
-    }
-    #ticket-reply-form {
-        position: absolute;
-        height: 43px;
-        bottom: 0;
-        right: 15px;
-        left: 15px;
-    }
-
-    div.input-box {
-        background-color: beige;
-        align-items: center;
-    }
-
-    div.input-box svg {
-        width: 30px;
-        height: 30px;
-    }
-
-    div.input-box svg.upload {
-        margin-left:10px;
-    }
-
-    div.file-preview {
-        position: fixed;
-        bottom: 10vh;
-        width: 80vw;
-        z-index: 9999;
-        display: block;
-        background-color: white;
-        border-radius: 3px;
     }
 </style>
 @section('panel_content')
-    <div class="card chat">
-        <div class="card-header row gutters-5">
-            <div class="text-center text-md-left">
-                <h5 class="mb-md-0 h5">Tiktok Shop Serve</h5>
-               <div class="mt-2">
-                   <span> {{ $ticket->user->name }} </span>
-               </div>
-            </div>
-        </div>
-        <div class="card-body msg-box" style="padding: 0!important;">
-            <div class="pad-top">
-                <ul class="list-group list-group-flush ticket">
-                    @foreach($ticket_replies as $ticketreply)
-                        <li class="list-group-item px-0 {{$ticketreply->user->id == Auth::id() ? 'mine' : ''}}" data-id="{{$ticketreply->id}}">
-                            @if(!empty($ticketreply->reply) || $ticketreply->files)
-                            <div class="media">
-                                <div class="media-body">
-                                    <div class="comment-header">
-                                        <span class="text-bold h6 text-muted title">
-                                            @php echo empty($ticketreply->user_id) || -1 == $ticketreply->user_id ? translate($ticketreply->reply) : $ticketreply->reply; @endphp
-                                            @if($ticketreply->files)
-                                                <div class="images {{$ticketreply->user->id == Auth::id() ? 'mine' : ''}}">
-                                                    @if (strpos($ticketreply->files, "base64") === false)
-                                                    @foreach ((explode(",",$ticketreply->files)) as $key => $file)
+    <div class="chatroot">
+        <div class="main">
+            <div class="kefuroot">
+                <div>
+                    <div class="chatlist">
+                        @foreach($ticket_replies as $index => $ticketreply)
+                            <div class="chat {{$ticketreply->user->id == Auth::id() ? 'mine' : ''}}" data-id="{{$ticketreply->id}}">
+                                @if($index % 4 === 0)
+                                    <div class="timeIndex">{{substr($ticketreply->created_at, 5, -3)}}</div>
+                                @endif
 
-                                                        @php $file_detail = \App\Models\Upload::where('id', $file)->first(); @endphp
-                                                        @if($file_detail != null)
-                                                            <img src="{{ static_asset('assets/img/placeholder.jpg') }}" data-src="{{ uploaded_asset($file) }}" onclick="previewImg(this)" class="mr-3 lazyload size-100px img-fit rounded" alt="Image">
-                                                        @endif
-                                                    @endforeach
-                                                    @else
-                                                        <img src="{{$ticketreply->files}}" data-src="{{$ticketreply->files}}" onclick="previewImg(this)" class="mr-3 lazyload size-100px img-fit rounded" alt="Image">
-                                                    @endif
-                                                </div>
-                                            @endif
-                                            <p class="text-muted text-sm fs-11 time">{{date('m-d H:i', strtotime($ticketreply->created_at))}}</p>
-                                        </span>
+                                @if($ticketreply->user->id == Auth::id())
+                                    <div class="btext">
+                                        @if($ticketreply->files)
+                                            @foreach ((explode(",",$ticketreply->files)) as $key => $file)
+                                                <img class="chatImg lazyload" src="{{ static_asset('assets/img/chat/placeholder.jpg') }}" data-src="{{ uploaded_asset($file) }}" onclick="previewImg(this)" />
+                                            @endforeach
+                                        @endif
+                                        @if($ticketreply->reply)
+                                            <span class="bspan">@php echo empty($ticketreply->user_id) || -1 == $ticketreply->user_id ? translate($ticketreply->reply) : $ticketreply->reply; @endphp</span>
+                                        @endif
+                                        <img src="{{Auth::user()->avatar_original ? uploaded_asset(Auth::user()->avatar_original) : static_asset('assets/img/chat/chat/head2.png')}}" class="head" style="margin-left: 8px;" />
                                     </div>
-                                </div>
+                                @else
+                                    @if(empty($ticketreply->user_id))
+                                        <div class="pic">
+                                            <div class="welcome-msg">
+                                                <svg t="1685791561209" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3147" width="16" height="16"><path d="M809.6 416.64h-53.76V308.48c0-135.68-108.096-243.84-243.2-243.84-135.04 0-243.2 108.16-243.2 243.84v108.16h-53.76c-29.44 0-53.76 24.32-53.76 53.76v433.28c0 29.44 24.32 53.76 53.76 53.76h593.92c30.08 0 54.4-24.32 54.4-53.76V470.4c0-29.44-24.32-53.76-54.4-53.76z m-135.04 0H350.72V308.48c0-89.6 72.96-162.56 161.92-162.56a162.56 162.56 0 0 1 161.92 162.56v108.16z" fill="#040000" p-id="3148"></path></svg>
+                                                {{translate($ticketreply->reply)}}！
+                                            </div>
+                                        </div>
+                                    @else
+                                        <div class="atext">
+                                            <img src="{{static_asset('assets/img/chat/head1.png')}}" class="head" style="margin-right: 8px;" />
+                                            @if($ticketreply->files)
+                                                @foreach ((explode(",",$ticketreply->files)) as $key => $file)
+                                                    <img class="chatImg lazyload" src="{{ static_asset('assets/img/chat/placeholder.jpg') }}" data-src="{{ uploaded_asset($file) }}" onclick="previewImg(this)" />
+                                                @endforeach
+                                            @endif
+                                            @if($ticketreply->reply)
+                                                <span class="aspan">@php echo empty($ticketreply->user_id) || -1 == $ticketreply->user_id ? translate($ticketreply->reply) : $ticketreply->reply; @endphp</span>
+                                            @endif
+                                        </div>
+                                    @endif
+                                @endif
                             </div>
-                            @endif
-                        </li>
-                    @endforeach
-                </ul>
-                <form action="{{ route('seller.support_ticket.reply_store') }}" method="post" id="ticket-reply-form" enctype="multipart/form-data">
-                    @csrf
-                    <input type="hidden" name="ticket_id" value="{{$ticket->id}}" required>
-                    <input type="hidden" name="user_id" value="{{$ticket->user_id}}">
+                        @endforeach
+                    </div>
 
-                    <div class="form-group row">
-                        <div class="col-md-12">
-                            <div class="input-group" data-toggle="aizuploader" data-type="image" data-multiple="true">
-                                <div class="input-group-prepend">
-                                    <div class="input-group-text bg-soft-secondary font-weight-medium">{{ translate('Browse')}}</div>
-                                </div>
-                                <div class="form-control file-amount">{{ translate('Choose File') }}</div>
-                                <input type="hidden" name="attachments" class="selected-files">
-                            </div>
-                            <div class="file-preview box sm">
-                            </div>
+                </div>
+                <div class="botRoot">
+                    <div class="file-preview box sm"></div>
+                    <div class="bot">
+                        <div class="message fujian" onclick="openFileSelection()"><img src="{{static_asset('assets/img/chat/fujian.jpeg')}}" width="30"/></div>
+
+                        <form id="ticket-reply-form" action="{{route('seller.support_ticket.reply_store')}}" method="POST" enctype="multipart/form-data" style="display: flex;flex:1">
+                            @csrf
+                            <input type="hidden" name="ticket_id" value="{{$ticket->id}}" required>
+                            <input type="hidden" name="user_id" value="{{$ticket->user_id}}">
+                            <input type="hidden" name="attachments" class="selected-files">
+                            <input type="file" id="fileInput" name="file" style="display: none;" accept="image/*" />
+                            <input class="input" placeholder="{{translate('Please enter your question')}}" name="reply" onkeydown="submitReply()" enterkeyhint="send" />
+                        </form>
+
+                        <div class="senddiv">
+                            <div class="message send" onclick="submit_reply('pending')">{{translate('Send')}}</div>
+                            <div class="message loading" style="display: none;"><img src="{{static_asset('assets/img/loading.gif')}}" /> </div>
                         </div>
+
                     </div>
-                    <div class="form-group row">
-                        <div class="col-md-10">
-                            <input id="reply-input" class="form-control" type="text" name="reply" value="" />
-                        </div>
-                        <div class="col-md-2">
-                            <button type="submit" class="btn btn-sm btn-primary" onclick="submit_reply('pending')">{{ translate('Send Reply') }}</button>
-                        </div>
-                    </div>
-                </form>
+                    <div class="sendBtn"></div>
+                </div>
+                <div class="notice">
+                    Tiktok Shop Serve
+                </div>
             </div>
         </div>
 
     </div>
 @endsection
+
 @section('script')
     @include('partials.support_ticket.script')
-
-    <script type="text/javascript">
-        $(document).ready(function () {
-            // $(".card.chat").height(document.body.clientHeight - 75);
-            // $(".aiz-main-content").height(document.body.clientHeight - 75);
-        });
-    </script>
 @endsection
