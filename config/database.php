7274ec0daa95ffa1ba0b5dc56f67dfc581451a08
mingming <449036002@qq.com>
2023-07-04 19:48:13 +0800

    连接数据库指定时区为配置文件中的设置的

--- config/database.php
+++ config/database.php
@@ -47,6 +47,7 @@ return [
             'username' => env('DB_USERNAME', ''),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
+            'timezone' => env('APP_TIMEZONE', 'UTC'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
