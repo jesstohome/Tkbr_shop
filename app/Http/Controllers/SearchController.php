3a868457db75174189dccee0fa8d8b55b575160d
mingming <449036002@qq.com>
2023-07-07 15:52:13 +0800

    优化前台 搜索，去掉店铺和分类的查询

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -136,44 +136,27 @@ class SearchController extends Controller
     public function ajax_search(Request $request)
     {
         $keywords = array();
-        $query = $request->search;
-        $products = Product::where('published', 1)->where('tags', 'like', '%' . $query . '%')->get();
-        foreach ($products as $key => $product) {
-            foreach (explode(',', $product->tags) as $key => $tag) {
-                if (stripos($tag, $query) !== false) {
-                    if (sizeof($keywords) > 5) {
-                        break;
-                    } else {
-                        if (!in_array(strtolower($tag), $keywords)) {
-                            array_push($keywords, strtolower($tag));
-                        }
-                    }
-                }
-            }
-        }
+        $query = trim($request->search ?? '');
 
+//        \DB::connection()->enableQueryLog();#开启执行日志
         $products = filter_products(Product::query());
-
         $products = $products->where('published', 1)->where("added_by", '!=',  'admin')
             ->where(function ($q) use ($query) {
-                foreach (explode(' ', trim($query)) as $word) {
-                    $q->where('name', 'like', '%' . $word . '%')
-                        ->orWhere('tags', 'like', '%' . $word . '%')
-                        ->orWhereHas('product_translations', function ($q) use ($word) {
-                            $q->where('name', 'like', '%' . $word . '%');
-                        })
-                        ->orWhereHas('stocks', function ($q) use ($word) {
-                            $q->where('sku', 'like', '%' . $word . '%');
-                        });
-                }
-            })
-            ->limit(3)
-            ->get();
+                $word = $query;
+                $q->where('name', 'like', '%' . $word . '%')
+                    ->orWhere('tags', 'like', '%' . $word . '%')
+                    ->orWhereHas('product_translations', function ($q) use ($word) {
+                        $q->where('name', 'like', '%' . $word . '%');
+                    })
+                    ->orWhereHas('stocks', function ($q) use ($word) {
+                        $q->where('sku', 'like', '%' . $word . '%');
+                    });
+            })->limit(3)->get();
 
-        $categories = Category::where('name', 'like', '%' . $query . '%')->get()->take(3);
+        $categories = [];
+        $shops = [];
 
-        $shops = Shop::whereIn('user_id', verified_sellers_id())->where('name', 'like', '%' . $query . '%')->get()->take(3);
-
+//        dd(\DB::connection()->getQueryLog());//打印SQL语句);
         if (sizeof($keywords) > 0 || sizeof($categories) > 0 || sizeof($products) > 0 || sizeof($shops) > 0) {
             return view('frontend.partials.search_content', compact('products', 'categories', 'keywords', 'shops'));
         }
