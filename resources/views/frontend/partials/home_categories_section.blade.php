6a1d893dbd9795e41efd11c24ba9112031252f18
mingming <449036002@qq.com>
2023-07-08 11:24:14 +0800

    手机端：优化一下商城首页制冷类的查看更多按钮

--- resources/views/frontend/partials/home_categories_section.blade.php
+++ resources/views/frontend/partials/home_categories_section.blade.php
@@ -9,7 +9,7 @@
             <div class="container">
                 <div class="px-2 py-4 px-md-4 py-md-3 bg-white shadow-sm rounded" style="padding-bottom: 0 !important;">
                     <div class="d-flex mb-3 align-items-baseline border-bottom">
-                        <h3 class="h5 fw-700 mb-0">
+                        <h3 class="h5 fw-700 mb-0" style="flex: 1;">
                             <span class="border-bottom border-primary border-width-2 pb-3 d-inline-block">{{ $category->getTranslation('name') }}</span>
                         </h3>
                         <a href="{{ route('products.category', $category->slug) }}" class="ml-auto mr-0 btn btn-primary btn-sm shadow-md">{{ translate('View More') }}</a>
