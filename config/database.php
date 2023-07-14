4d6ddf4e0fdd6cb8380e03936b1acc075577bc0d
mingming <449036002@qq.com>
2023-07-15 01:37:39 +0800

    去掉数据库的时区设置

--- config/database.php
+++ config/database.php
@@ -47,7 +47,6 @@ return [
             'username' => env('DB_USERNAME', ''),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'timezone' => '+08:00',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
