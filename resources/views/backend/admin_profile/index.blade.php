9c6ca62fb300fd5dcbae46b713ac00e473160a34
mingming <449036002@qq.com>
2023-07-16 15:27:28 +0800

    所有服--密码小眼睛，在不同的页面情况下（后缀不一样的情况下），有的有显示，有的没有显示

--- resources/views/backend/admin_profile/index.blade.php
+++ resources/views/backend/admin_profile/index.blade.php
@@ -25,14 +25,16 @@
                     </div>
                     <div class="form-group row">
                         <label class="col-sm-3 col-from-label" for="new_password">{{translate('New Password')}}</label>
-                        <div class="col-sm-9">
+                        <div class="col-sm-9" style="display: flex">
                             <input type="password" class="form-control" placeholder="{{translate('New Password')}}" name="new_password">
+                            <div class="u-success u-eye"></div>
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-sm-3 col-from-label" for="confirm_password">{{translate('Confirm Password')}}</label>
-                        <div class="col-sm-9">
+                        <div class="col-sm-9" style="display: flex">
                             <input type="password" class="form-control" placeholder="{{translate('Confirm Password')}}" name="confirm_password">
+                            <div class="u-success u-eye"></div>
                         </div>
                     </div>
                     <div class="form-group row">
