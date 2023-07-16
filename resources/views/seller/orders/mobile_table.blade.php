439bc837745882fa31aacf52a3c3172e1cbddf89
mingming <449036002@qq.com>
2023-07-16 20:32:18 +0800

    卖家端-订单-页面UI优化

--- resources/views/seller/orders/mobile_table.blade.php
+++ resources/views/seller/orders/mobile_table.blade.php
@@ -65,40 +65,40 @@
         <div class="order-btns">
             <div class="status">
                 @if($order->delivery_status == 'cancelled')
-                    <span class="badge badge-inline badge-danger">{{translate('Cancelled')}}</span>
+                    <span class="badge badge-inline badge-soft-danger">{{translate('Cancelled')}}</span>
                 @else
                     @if ($order->product_storehouse_status)
-                        <span class="badge badge-inline badge-success">{{translate('Picked Up')}}</span>
+                        <span class="badge badge-inline badge-soft-success">{{translate('Picked Up')}}</span>
                     @else
                         @if($order->paymentStatement)
-                            <span class="badge badge-inline badge-info">{{translate('Pending')}}</span>
+                            <span class="badge badge-inline badge-soft-info">{{translate('Pending')}}</span>
                         @else
                             @if ($order->product_storehouse_total)
-                                <span class="badge badge-inline badge-danger">{{translate('Unpicked Up')}}</span>
+                                <span class="badge badge-inline badge-soft-danger">{{translate('Unpicked Up')}}</span>
                             @endif
                         @endif
                     @endif
                 @endif
 
                 @if($order->delivery_status == 'cancelled')
-                    <span class="badge badge-inline badge-danger">{{translate('Cancelled')}}</span>
+                    <span class="badge badge-inline badge-soft-danger">{{translate('Cancelled')}}</span>
                 @else
                     @if ($order->payment_status == 'paid')
-                        <span class="badge badge-inline badge-success">{{ translate('Buyer has paid')}}</span>
+                        <span class="badge badge-inline badge-soft-success">{{ translate('Buyer has paid')}}</span>
                     @else
-                        <span class="badge badge-inline badge-danger">{{ translate('Unpaid')}}</span>
+                        <span class="badge badge-inline badge-soft-danger">{{ translate('Unpaid')}}</span>
                     @endif
                 @endif
             </div>
             <div class="bts">
-                <a href="{{ route('seller.orders.show', encrypt($order->id)) }}" class="btn btn-soft-info btn-sm" title="{{ translate('Order Details') }}" @if($order->delivery_status == 'cancelled') onclick="AIZ.plugins.notify('warning', '{{translate('The order has been cancelled')}}');return false;" @endif>
+                <a href="{{ route('seller.orders.show', encrypt($order->id)) }}" class="btn btn-info btn-sm" title="{{ translate('Order Details') }}" @if($order->delivery_status == 'cancelled') onclick="AIZ.plugins.notify('warning', '{{translate('The order has been cancelled')}}');return false;" @endif>
                     {{translate('View Detail')}}
                 </a>
-                <a href="{{ is_android() ? 'javascript:void(0);' : route('seller.invoice.download', $order->id) }}" class="btn btn-soft-warning btn-sm" title="{{ translate('Download Invoice') }}" @if (is_android()) onclick="downloadInvoicePdf('{{route('seller.invoice.download', $order->id)}}');" @endif>
+                <a href="{{ is_android() ? 'javascript:void(0);' : route('seller.invoice.download', $order->id) }}" class="btn btn-success btn-sm" title="{{ translate('Download Invoice') }}" @if (is_android()) onclick="downloadInvoicePdf('{{route('seller.invoice.download', $order->id)}}');" @endif>
                     {{translate('Download')}}
                 </a>
-                <a href="javascript:void(0);" class="btn btn-soft-info btn-sm" title="{{ translate('Order Details') }}" onclick="order_reply({{!empty($order->orderDetails[0]) ? $order->orderDetails[0]->product_id : 0}}, '{{!empty($order->orderDetails[0]->product->slug) ? route('product', $order->orderDetails[0]->product->slug) : ''}}', {{$order->seller_id}}, {{$order->user_id}}, '{{$order->user->name}}');">
-                    {{translate('Contact Service')}}
+                <a href="javascript:void(0);" class="btn btn-primary btn-sm" title="{{ translate('Order Details') }}" onclick="order_reply({{!empty($order->orderDetails[0]) ? $order->orderDetails[0]->product_id : 0}}, '{{!empty($order->orderDetails[0]->product->slug) ? route('product', $order->orderDetails[0]->product->slug) : ''}}', {{$order->seller_id}}, {{$order->user_id}}, '{{$order->user->name}}');">
+                    {{translate('Contact buyer')}}
                 </a>
             </div>
         </div>
@@ -108,7 +108,7 @@
                 <div>{{ translate('Order Type') }}</div>
                 <div>
                     @if ($order->order_type == 6)
-                        <span class="badge badge-inline badge-danger">{{ translate('Urgent') }}</span>
+                        <span class="badge badge-inline badge-soft-danger">{{ translate('Urgent') }}</span>
                     @elseif ($order->order_type == 24)
                         {{ translate('ordinary') }}
                     @else
