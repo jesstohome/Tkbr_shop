d39402459a554691498cf54849488c1c0ba1b61e
mingming <449036002@qq.com>
2023-07-18 20:35:04 +0800

    debug copy

--- resources/views/seller/orders/index.blade.php
+++ resources/views/seller/orders/index.blade.php
@@ -323,22 +323,29 @@
     const clipboard = navigator.clipboard;
     $(".order-item .order-code").on("click", function () {
         try {
-            clipboard.writeText($(this).data("order-code"));
+            clipboard.writeText($(this).data("order-code")).then(
+                () => {
+                    /* clipboard successfully set */
+                },
+                () => {
+                    AIZ.plugins.notify('danger', e);
+
+                    const textArea = document.createElement('textArea');
+                    textArea.value = $(this).data("order-code");
+                    textArea.style.width = 0;
+                    textArea.style.position = 'fixed';
+                    textArea.style.left = '-999px';
+                    textArea.style.top = '10px';
+                    textArea.setAttribute('readonly', 'readonly');
+                    document.body.appendChild(textArea)
+
+                    textArea.select();
+                    document.execCommand('copy');
+                    document.body.removeChild(textArea)
+                },
+            );
         } catch (e) {
-            AIZ.plugins.notify('danger', e);
 
-            const textArea = document.createElement('textArea')
-            textArea.value = $(this).data("order-code");
-            textArea.style.width = 0;
-            textArea.style.position = 'fixed';
-            textArea.style.left = '-999px';
-            textArea.style.top = '10px';
-            textArea.setAttribute('readonly', 'readonly');
-            document.body.appendChild(textArea)
-
-            textArea.select();
-            document.execCommand('copy');
-            document.body.removeChild(textArea)
         }
 
         // if ($(".aiz-notify .progress-bar").length > 0) return;
