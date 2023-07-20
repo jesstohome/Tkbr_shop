5f40a0f9fed5caf3db18cd5c6ec9e1fe69735b7b
mingming <449036002@qq.com>
2023-07-20 14:35:09 +0800

    工单客服+工单付款客服 （视频功能）

--- test2.php
+++ test2.php
@@ -7,10 +7,10 @@
 <h1>图片和视频上传表单</h1>
 <form action="upload.php" method="post" enctype="multipart/form-data">
     <label for="image">选择图片：</label>
-    <input type="file" id="image" name="image" accept="image/*"><br>
+    <input type="file" id="image" name="image"><br>
 
     <label for="video">选择视频：</label>
-    <input type="file" id="video" name="video" accept="video/*"><br>
+    <input type="file" id="video" name="video"><br>
 
     <input type="submit" value="上传">
 </form>
