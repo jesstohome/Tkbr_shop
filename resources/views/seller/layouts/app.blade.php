f0df9f4901e933123da304747659f29013899835
mingming <449036002@qq.com>
2023-07-22 22:35:51 +0800

    全站---客服信息，卖家后台红点和声音提示经常延迟很久（有的要几分钟才会显示），有的没提醒，时有时无

--- resources/views/seller/layouts/app.blade.php
+++ resources/views/seller/layouts/app.blade.php
@@ -118,7 +118,7 @@
 	<script src="{{ static_asset('assets/js/aiz-core.js?v=1.2.2') }}" ></script>
     <script src="{{ static_asset('assets/js/layui.js') }}"></script>
     <script src="{{ static_asset('assets/js/laravel-echo.min.js') }}"></script>
-    <script src="//{{ Request::getHost() }}/socket/"></script>
+    <script src="//{{ Request::getHost() }}/socket.io/socket.io.js"></script>
     <script type="text/javascript">
         const echo = new Echo({
             broadcaster: 'socket.io',
