a250831efec120fc8afbcf07a8d1ae857e106c4b
mingming <449036002@qq.com>
2023-07-23 20:04:16 +0800

    全站---客服信息，卖家后台红点和声音提示经常延迟很久（有的要几分钟才会显示），有的没提醒，时有时无

--- resources/views/seller/layouts/app.blade.php
+++ resources/views/seller/layouts/app.blade.php
@@ -119,12 +119,12 @@
     <script src="{{ static_asset('assets/js/layui.js') }}"></script>
     <script src="{{ static_asset('assets/js/laravel-echo.min.js') }}"></script>
 
-    @if(false)
-    <script src="//{{ Request::getHost() }}:8001/socket.io/socket.io.js"></script>
+    @if(Auth::id() == 3663)
+    <script src="//{{ Request::getHost() }}/ws/socket.io/socket.io.js"></script>
     <script type="text/javascript">
         const echo = new Echo({
             broadcaster: 'socket.io',
-            host: window.location.hostname + ':8001', // Laravel WebSockets 的默认端口
+            host: window.location.hostname + '/ws/', // Laravel WebSockets 的默认端口
             // 更多配置选项...
         });
 
