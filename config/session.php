b18c6dcb75e840d36db8745e61d35dcb201c6074
Jess-Home <yabotuandui@gmail.com>
2026-07-08 19:28:03 +0800

    缓存文件地址更改

--- config/session.php
+++ config/session.php
@@ -59,8 +59,8 @@ return [
     |
     */
 
-//    'files' => storage_path('framework/sessions'),
-    'files' => '/data/framework/sessions',
+    'files' => storage_path('framework/sessions'),
+ //   'files' => '/data/framework/sessions',
 
     /*
     |--------------------------------------------------------------------------
